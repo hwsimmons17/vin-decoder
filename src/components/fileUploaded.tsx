b4cfd072b265c688a48fa0e9d431f853efaf38ed
Hunter Simmons <hunterwilliamsimmons@gmail.com>
2023-06-06 16:00:44 -0600

    Add copied to clipboard

--- src/components/fileUploaded.tsx
+++ src/components/fileUploaded.tsx
@@ -3,6 +3,7 @@ import {
   CheckCircleIcon,
   ClipboardDocumentCheckIcon,
   ClipboardDocumentIcon,
+  XMarkIcon,
 } from "@heroicons/react/24/outline";
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
@@ -19,18 +20,34 @@ export default function FileUploaded({
   imageFiles: FileList;
   clearData: () => void;
 }) {
+  const [displayCopied, setDisplayCopied] = useState(false);
+
+  const copy = (text: string) => {
+    navigator.clipboard.writeText(text);
+    setDisplayCopied(true);
+    setTimeout(() => setDisplayCopied(false), 1000);
+  };
   return (
     <>
       <div className="">
+        {displayCopied && <CopiedCode />}
         {Array.from(imageFiles).map((imageFile) => {
-          return <CardElement file={imageFile} key={imageFile.name} />;
+          return (
+            <CardElement file={imageFile} key={imageFile.name} copy={copy} />
+          );
         })}
       </div>
     </>
   );
 }
 
-function CardElement({ file }: { file: File }) {
+function CardElement({
+  file,
+  copy,
+}: {
+  file: File;
+  copy: (text: string) => void;
+}) {
   const [image, setImage] = useState<vinImage>({
     file,
     key: file.name,
@@ -56,7 +73,7 @@ function CardElement({ file }: { file: File }) {
     <button
       className="relative w-full rounded-lg border-2 border-dashed border-gray p-2 text-center hover:border-gray-400 mt-6"
       key={image.key}
-      onClick={() => navigator.clipboard.writeText(image.vin ?? "")}
+      onClick={() => copy(image.vin ?? "")}
     >
       <div>
         <div className="flex item-center justify-between">
@@ -86,6 +103,37 @@ function CardElement({ file }: { file: File }) {
         )}
       </div>
     </button>
+  );
+}
+
+function CopiedCode() {
+  return (
+    <div className="rounded-md bg-green-50 p-4 mt-6">
+      <div className="flex">
+        <div className="flex-shrink-0">
+          <CheckCircleIcon
+            className="h-5 w-5 text-green-400"
+            aria-hidden="true"
+          />
+        </div>
+        <div className="ml-3">
+          <p className="text-sm font-medium text-green-800">
+            Copied to clipboard
+          </p>
+        </div>
+        <div className="ml-auto pl-3">
+          <div className="-mx-1.5 -my-1.5">
+            <button
+              type="button"
+              className="inline-flex rounded-md bg-green-50 p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50"
+            >
+              <span className="sr-only">Dismiss</span>
+              <XMarkIcon className="h-5 w-5" aria-hidden="true" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
