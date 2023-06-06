9d62e15240522e95dc5a4cd56332e552ad688d5c
Hunter Simmons <hunterwilliamsimmons@gmail.com>
2023-06-06 15:53:09 -0600

    App works

--- src/components/fileUploaded.tsx
+++ src/components/fileUploaded.tsx
@@ -1,5 +1,16 @@
-import { ArchiveBoxIcon, CheckCircleIcon } from "@heroicons/react/24/outline";
-import { Dispatch, SetStateAction } from "react";
+import {
+  ArchiveBoxIcon,
+  CheckCircleIcon,
+  ClipboardDocumentCheckIcon,
+  ClipboardDocumentIcon,
+} from "@heroicons/react/24/outline";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+
+type vinImage = {
+  key: string;
+  file: File;
+  vin: string | null;
+};
 
 export default function FileUploaded({
   imageFiles,
@@ -12,46 +23,69 @@ export default function FileUploaded({
     <>
       <div className="">
         {Array.from(imageFiles).map((imageFile) => {
-          return (
-            <button
-              className="relative w-full rounded-lg border-2 border-dashed border-gray p-2 text-center hover:border-gray-400 mt-6 flex item-center justify-between"
-              key={imageFile.name}
-            >
-              <div className="flex justify-center item-center mt-1.5">
-                <ArchiveBoxIcon className="h-6 w-6 text-gray m-0 mx-2" />
-                <span className="text-sm font-regular text-gray flex item-center align-baseline inline-block mx-2">
-                  {imageFile.name}
-                </span>
-              </div>
-              <div className="h-6">
-                {true ? (
-                  <CheckCircleIcon className="h-6 w-6 text-green-600 mt-1" />
-                ) : (
-                  <Spinner />
-                )}
-              </div>
-            </button>
-          );
+          return <CardElement file={imageFile} key={imageFile.name} />;
         })}
-
-        {/* <div className="flex pt-6 place-content-between">
-          <button
-            type="button"
-            className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-            onClick={clearData}
-          >
-            Cancel
-          </button>
-          <button
-            disabled={!true}
-            type="button"
-            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Submit
-          </button>
-        </div> */}
       </div>
     </>
+  );
+}
+
+function CardElement({ file }: { file: File }) {
+  const [image, setImage] = useState<vinImage>({
+    file,
+    key: file.name,
+    vin: null,
+  });
+
+  const getVin = async (file: File) => {
+    var formData = new FormData();
+    formData.append("image", file);
+    const response = await fetch("/api/vin", {
+      method: "POST",
+      body: formData,
+    });
+
+    let vinRes = await response.json();
+    setImage({ file, key: file.name, vin: vinRes.vinNumber });
+  };
+
+  useEffect(() => {
+    getVin(file);
+  }, []);
+  return (
+    <button
+      className="relative w-full rounded-lg border-2 border-dashed border-gray p-2 text-center hover:border-gray-400 mt-6"
+      key={image.key}
+      onClick={() => navigator.clipboard.writeText(image.vin ?? "")}
+    >
+      <div>
+        <div className="flex item-center justify-between">
+          <div className="flex justify-center item-center mt-1.5">
+            <ArchiveBoxIcon className="h-6 w-6 text-gray m-0 mx-2" />
+            <span className="text-sm font-regular text-gray flex item-center align-baseline inline-block mx-2">
+              {image.key}
+            </span>
+          </div>
+          <div className="h-6">
+            {image.vin ? (
+              <CheckCircleIcon className="h-6 w-6 text-green-600 mt-1" />
+            ) : (
+              <Spinner />
+            )}
+          </div>
+        </div>
+        {image.vin && (
+          <div className="flex item-center justify-center mt-2">
+            <ClipboardDocumentIcon className="h-6 w-6" />
+            <div className="flex justify-center item-center mt-1.5">
+              <span className="text-sm font-regular text-gray flex item-center align-baseline inline-block mx-2">
+                Vin Number: {image.vin}
+              </span>
+            </div>
+          </div>
+        )}
+      </div>
+    </button>
   );
 }
 
